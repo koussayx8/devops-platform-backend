1c8c52ddf52516b8ec63571df0e121ea85142713
Koussay Belhouchet <belhouchet.koussay@esprit.tn>
2025-09-15 15:45:39 +0100

    Remove H2 database dependency from pom.xml and enhance test configuration with mock repositories for improved unit testing.

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -2,11 +2,34 @@ package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import tn.esprit.spring.repositories.*;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration",
+    "server.port=0"
+})
 class GestionStationSkiApplicationTests {
+
+    @MockBean
+    private ISkierRepository skierRepository;
+    
+    @MockBean
+    private IPisteRepository pisteRepository;
+    
+    @MockBean
+    private ICourseRepository courseRepository;
+    
+    @MockBean
+    private IRegistrationRepository registrationRepository;
+    
+    @MockBean
+    private ISubscriptionRepository subscriptionRepository;
+    
+    @MockBean
+    private IInstructorRepository instructorRepository;
 
     @Test
     void contextLoads() {
